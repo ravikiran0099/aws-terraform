1a7f3a175980a58a37c607ff3bf6b25914b8dc73
ravikiran0099 <129965804+ravikiran0099@users.noreply.github.com>
2023-04-07 10:09:11 -0400

    keys deleted

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAW5AW63DBVLTHTSXR"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
